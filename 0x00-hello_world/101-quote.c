7ef0dce4c9e150a9bc00b13b3a526ab434a92ff0
Enoch Ayivi <ayivienoch>
2022-09-14 19:16:38 -0700

    compiler

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 /**
- * main Entry point
+ * main - Entry point
  *
- * Description: 'the program's description'
+ * Description: 'the prgram's description'
  *
- * Return: will be 1 on success
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	retunrn (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
