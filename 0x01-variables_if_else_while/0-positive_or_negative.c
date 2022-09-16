018b42493423e96531cdded9782a47db5d9daecf
Enoch Ayivi <ayivienoch>
2022-09-16 14:19:23 -0700

    Insomnia

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - print if the number is positve, zero, or negative
  *
@@ -11,11 +10,11 @@
  */
 int main(void)
 {
+
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	
 if (n > 0)
 {
