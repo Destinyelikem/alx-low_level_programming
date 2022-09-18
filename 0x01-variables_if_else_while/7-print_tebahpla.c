3ab9ade2e17fa743f6a3cbf3049eed267d65082b
Enoch Ayivi <ayivienoch>
2022-09-18 15:32:45 -0700

    smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	purchar('\n');
+
+	return (0);
+}
