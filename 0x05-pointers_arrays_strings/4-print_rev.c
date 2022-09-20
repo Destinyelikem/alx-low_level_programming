570349dd7b504d30f7d06c09b812adfc433097c9
Enoch Ayivi <ayivienoch>
2022-09-19 17:54:52 -0700

    All task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "main,h"
+#include "main.h"
 
 /** print_rev - This function prints a reverse string
  *
@@ -8,7 +8,9 @@ void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index--)
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
 	}
