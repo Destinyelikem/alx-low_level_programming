8c13fac75919b33a2d6dedc1e4cbb80372e55cdd
Enoch Ayivi <ayivienoch>
2022-09-22 14:53:39 -0700

    All task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 
 /**
  * _strlen - Function that returns the length of a string
+ *
  * @s: this is the input string
  *
  * Return: Lenngth of thr string
  */
-
 int _strlen(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0' ; index++);
+	for (index = 0; s[index] != '\0' ; index++)
 
 	return (index);
 }
