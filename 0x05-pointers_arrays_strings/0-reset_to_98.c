71a12541323afa691c3cd0b25f1a382cac7718fc
Enoch Ayivi <ayivienoch>
2022-09-22 14:19:45 -0700

    All task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,6 @@
  * reset_t0_98 - Write a function that takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: This is the entry
- *
  * Return: void
  */
 void reset_to_98(int *n)
