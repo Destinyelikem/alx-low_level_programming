bb5d0ed68a96b39b39d2d7a43f1627400e661019
Enoch Ayivi <ayivienoch>
2022-09-22 13:16:11 -0700

    All task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  *  _strcmp - function which compare two strings and
  *@s1: first string
  *@s2:second string
- *Return: 
+ *Return: int value
  *returns zero if s1 == s2
  *returns negative number if s1 < s2
  *returns positive number if s1 > s2
