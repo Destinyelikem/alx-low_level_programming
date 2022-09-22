4c754a91ebcdc734bcbf8fbc0e248f5e9d00b769
Enoch Ayivi <ayivienoch>
2022-09-22 14:39:13 -0700

    All task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_t0_98 - updates the value it points to to 98.
+ * reset_to_98 - updates the value it points to to 98.
  * @n: This is the entry
  *
  * Return: void
