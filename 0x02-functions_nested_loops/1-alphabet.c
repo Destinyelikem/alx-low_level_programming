6914f33bee0d0213feaaa6bbe6064bde0fc1979a
Enoch Ayivi <ayivienoch>
2022-09-22 20:18:13 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: Always success
+ */
+void print_alphabet(void)
+{
+	int n;
+	
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchr(n);
+	}
+	_putchar('\n');
+}
