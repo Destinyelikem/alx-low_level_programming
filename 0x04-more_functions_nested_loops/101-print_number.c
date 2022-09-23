ddcaf4057232d1f3c83c3d7e461d250c8a6fc2e3
Enoch Ayivi <ayivienoch>
2022-09-23 16:21:28 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: the integer is to be printed
+ * Return: void
+ */
+void print_number
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
