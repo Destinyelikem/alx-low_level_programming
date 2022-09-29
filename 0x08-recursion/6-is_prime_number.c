25f4d8579e5b5f29fe315d36656a49ca6356e32b
Enoch Ayivi <ayivienoch>
2022-09-28 20:04:03 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,10 +14,14 @@ int helperFunction(int num, int i)
 		{
 			return (0);
 		}
+		else
+		{
+			return (helperFunction(num, i + 1));
+		}
 	}
 	else
 	{
-		return (helperFunction(num, i + 1));
+		return (1);
 	}
 }
 
